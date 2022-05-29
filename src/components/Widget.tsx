c8dd4ea89de0773e7550b7bbff57367f168da7c1
Thiago Mata <taiagomata@gmail.com>
2022-05-29 01:46:39 -0300

    headlessui added

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,20 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { ChatTeardropDots } from "phosphor-react";
+import { Popover } from '@headlessui/react'
 
 const Widget = () => {
+
+
   return (
 
-    <div className="absolute bottom-4 right-4">
-      <button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">       
+    <Popover className="absolute bottom-4 right-4">
+      <Popover.Panel>Hello World</Popover.Panel>
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">       
         <ChatTeardropDots className="w-6 h-6" />
-
 
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
           <span className="pl-3"></span>
           Feedback
         </span>
-      </button>
-    </div>
+      </Popover.Button>
+    </Popover>
       
 
   );
